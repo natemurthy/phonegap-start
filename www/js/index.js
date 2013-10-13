7dfbc1b96765730bc45088da259606fd45368d3f
nasty nate <nate@nathan-macbook.local>
2013-10-12 18:06:14 -0700

    added orange Aloma theme and ring button page

--- www/js/index.js
+++ www/js/index.js
@@ -32,9 +32,40 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
+	errorCB: function(err) {
+        console.log("Error processing SQL: "+err.code);
+    },
+    
+	initializeDb: function(tx) {
+    	tx.executeSql('CREATE TABLE IF NOT EXISTS NAME (id unique, name)');
+    },
+    
+    querySuccess: function(tx, results) {
+        console.log("Returned rows = " + results.rows.length);
+        // this will be true since it was a select statement and so rowsAffected was 0
+        if (!results.rowsAffected) {
+            console.log('No rows affected!');
+            return false;
+        }
+        // for an insert statement, this property will return the ID of the last inserted row
+        console.log("Last inserted row ID = " + results.insertId);
+    },
+    
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        var RingMeDb = window.openDatabase("RingMe", "1.0", "RingMe", 10);
+        RingMeDb.transaction(initializeDb,errorCB);
     },
+    
+    createName: function(tx) {
+    	var name = document.getElementById("name").value;
+    	var RingMeDb = window.openDatabase("RingMe", "1.0", "RingMe", 10);
+    	RingMeDb.transaction(function(tx) {
+    		tx.executeSql('INSERT INTO NAME (id,name) VALUES (1,'+name+')');
+    	},errorCB);
+    	
+    }
+    
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
